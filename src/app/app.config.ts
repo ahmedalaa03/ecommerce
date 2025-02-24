aad7acb89d1bc1bae62a560c06576cfe27b3010b
ahmedalaa03 <ahmedalaa03@gmail.com>
2025-02-24 20:38:16 +0200

    hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,22 @@
+import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { NgxSpinnerModule } from "ngx-spinner";
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { headersInterceptor } from './core/interceptors/headers/headers.interceptor';
 import { errorsInterceptor } from './core/interceptors/errors/errors.interceptor';
-import { NgxSpinnerModule } from "ngx-spinner";
+import { headersInterceptor } from './core/interceptors/headers/headers.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading/loading.interceptor';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, '/i18n/', '.json');
 }
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,withViewTransitions(),withHashLocation()),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,withViewTransitions()),
   provideClientHydration(withEventReplay()),
   provideHttpClient(withFetch(), withInterceptors([headersInterceptor, errorsInterceptor, loadingInterceptor])),
   provideAnimations(),
